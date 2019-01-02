129abee24508137927c6ed9dd95669b391a83b18
pedromassango <pedromassango.developer@gmail.com>
2019-01-02 12:28:29 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Flutter Sample'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+
+  int _currentIndex = 0;
+
+  List<Widget> _tabList = [
+    Container(
+      color: Colors.teal,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.purple,
+    )
+
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: _tabList.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (currentIndex){
+
+          setState(() {
+            _currentIndex = currentIndex;
+          });
+
+            _tabController.animateTo(_currentIndex);
+
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            title: Text("Files"),
+            icon: Icon(Icons.folder)
+          ),
+          BottomNavigationBarItem(
+            title: Text("Settings"),
+            icon: Icon(Icons.settings)
+          )
+        ],
+      ),
+    );
+  }
+}
